ed67f41a74fd59a597a7befd600c486f0a3a3744
mike <michael.dez@gmail.com>
2022-02-10 21:51:04 -0700

    changed font, added p10k

--- unpack.sh
+++ unpack.sh
@@ -6,4 +6,6 @@ ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
 ln -s ${BASEDIR}/vim/vim ~/.vim
 ln -s ${BASEDIR}/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ${BASEDIR}/zsh/zshrc ~/.zshrc
+ln -s ${BASEDIR}/zsh/p10k.zsh ~/.p10k.zsh
 ln -s ${BASEDIR}/zsh/zshenv ~/.zshenv
+ln -s ${BASEDIR}/zsh/oh-my-zsh ~/.oh-my-zsh
